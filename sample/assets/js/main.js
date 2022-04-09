1d45bb7412c7a2fc949b27286d411f0c0f959ba7
TatsuyaMatsumoto <tatsuya.11831@gmail.com>
2022-04-09 21:50:16 +0900

    修正

--- sample/assets/js/main.js
+++ sample/assets/js/main.js
@@ -1,1 +1,34 @@
+$("document").ready(function(){
+  $('.l-mainShop__sellerItem').slick({
+    infinite: true,
+    dots:false,
+    slidesToScroll: 1,
+    arrows: true,
+    slidesToShow: 4,
+    responsive:[
+      {
+        breakpoint:992,
+        setting:{
+          slidesToShow:2
+        },
+      },
+      {
+        breakpoint:768,
+        setting:{
+          slidesToShow:1
+        },
+      },
+    ],
+  });
+});
 
+$(document).ready(function() {
+  $('#toggle').on('click', function() {
+    $('body').addClass('is-open');
+    $('body').addClass('overlay');
+  });
+  $('#closeBtn').on('click', function() {
+    $('body').removeClass('is-open');
+    $('body').removeClass('overlay');
+  });
+});
